99a13c44764c0ba4f306ca76f44ca46d80cf5392
Yang Minghui <minghuiyang1998@gmail.com>
2019-05-13 15:21:59 +0800

    [+]init

---
+++ routes/index.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+var env = process.env.NODE_ENV || "production";
+const { Pool } = require('pg')
+const pool = new Pool()
+
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err)
+  process.exit(-1)
+})
+
+var config = {
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "yangminghui"
+};
+
+var conString = `postgres://${config.user}:${config.password}@${config.host}/${config.database}`;
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index');
+});
+
+
+router.get('/films', function (req, res, next) {
+  pool.connect((err, client, done) => {
+    if (err) throw err
+    client.query('SELECT * FROM films_all', (err, result) => {
+      done()
+
+      if (err) {
+        console.log(err.stack)
+      } else {
+        let films = result.rows.map((item) => {
+          Object.keys(item).forEach((key) => {
+            if (item[key] && item[key].match(/^\[.*\]$/sg)) {
+              item[key] = JSON.parse(item[key])
+            }
+          })
+          return item
+        })
+        res.send({ films });
+      }
+    })
+  })
+
+});
+
+module.exports = router;
