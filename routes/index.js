7cf93075cd397dfb20c14da17f28f97055a59580
Yang Minghui <minghuiyang1998@gmail.com>
2019-05-14 09:19:29 +0800

    [+]deploy about local database

--- routes/index.js
+++ routes/index.js
@@ -2,37 +2,28 @@ var express = require('express');
 var router = express.Router();
 var env = process.env.NODE_ENV || "production";
 const { Pool } = require('pg')
-const pool = new Pool()
-
-// pool使用默认的localhost配置
-pool.on('error', (err, client) => {
-  console.error('Unexpected error on idle client', err)
-  process.exit(-1)
+const pool = new Pool({
+  database: 'webhw',
 })
 
 /* GET home page. */
 router.get('/films', function (req, res, next) {
-  pool.connect((err, client, done) => {
-    if (err) throw err
-    client.query('SELECT * FROM films_all', (err, result) => {
-      done()
-
-      if (err) {
-        console.log(err.stack)
-      } else {
-        let films = result.rows.map((item) => {
-          Object.keys(item).forEach((key) => {
-            if (item[key] && item[key].match(/^\[.*\]$/g)) {
-              item[key] = JSON.parse(item[key])
-            }
-          })
-          return item
+  pool.query('SELECT * FROM films_all', (err, result) => {
+    if (err) console.log(err)
+    if (result) {
+      let films = result.rows.map((item) => {
+        Object.keys(item).forEach((key) => {
+          if (item[key] && item[key].match(/^\[.*\]$/g)) {
+            item[key] = JSON.parse(item[key])
+          }
         })
-        res.send({ films });
-      }
-    })
+        return item
+      })
+      res.send({ films });
+    }
+    pool.end()
   })
-
 });
+
 
 module.exports = router;
